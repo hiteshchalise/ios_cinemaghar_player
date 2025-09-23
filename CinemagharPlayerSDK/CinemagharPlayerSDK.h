8f01302934cbc666b46ed8bd71a8eafc6c04e2c6
Hitesh Chalise <hiteshchalise10@gmail.com>
2025-09-23 08:21:04 +0545

    Initial Commit

---
+++ CinemagharPlayerSDK/CinemagharPlayerSDK.h
@@ -0,0 +1,18 @@
+//
+//  CinemagharPlayerSDK.h
+//  CinemagharPlayerSDK
+//
+//  Created by Hitesh Chalise on 23/09/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CinemagharPlayerSDK.
+FOUNDATION_EXPORT double CinemagharPlayerSDKVersionNumber;
+
+//! Project version string for CinemagharPlayerSDK.
+FOUNDATION_EXPORT const unsigned char CinemagharPlayerSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CinemagharPlayerSDK/PublicHeader.h>
+
+
---
+++ CinemagharPlayerSDKTests/CinemagharPlayerSDKTests.swift
@@ -0,0 +1,17 @@
+//
+//  CinemagharPlayerSDKTests.swift
+//  CinemagharPlayerSDKTests
+//
+//  Created by Hitesh Chalise on 23/09/2025.
+//
+
+import Testing
+@testable import CinemagharPlayerSDK
+
+struct CinemagharPlayerSDKTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
